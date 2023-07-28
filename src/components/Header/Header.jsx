fb7987f49a50305ddf06568b72439efc2e469f9b
ZhannaZucher <jzucher@gmail.com>
2023-07-28 12:38:37 +0200

    add: header & styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,24 @@
+import { Link } from "react-router-dom"
 import "./Header.css"
+import logo from "../../assets/argentBankLogo.png"
 
 export default function Header() {
-  return <div>HEADER</div>
+  return (
+    <nav className="main-nav">
+      <Link className="main-nav-logo" to="/">
+        <img
+          src={logo}
+          alt="logo Argent Bank"
+          className="main-nav-logo-image"
+        />
+        <h1 className="sr-only">Argent Bank</h1>
+      </Link>
+      <div>
+        <a className="main-nav-item" href="/sign-in">
+          <i className="fa fa-user-circle"></i>
+          Sign In
+        </a>
+      </div>
+    </nav>
+  )
 }
