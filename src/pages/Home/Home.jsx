80b72a5d036890044b5997428340ba08da094030
ZhannaZucher <jzucher@gmail.com>
2023-07-28 19:18:58 +0200

    move featuresContent to src/data

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,33 +1,7 @@
 import Hero from "../../components/Hero/Hero"
 import "./Home.css"
-import iconChat from "../../assets/icon-chat.png"
-import iconMoney from "../../assets/icon-money.png"
-import iconSecurity from "../../assets/icon-security.png"
+import { features } from "../../data/featuresContent"
 import FeatureItem from "../../components/FeatureItem/FeatureItem"
-
-const features = [
-  {
-    icon: iconChat,
-    alt: "Chat Icon",
-    title: "You are our #1 priority",
-    content:
-      "Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes.",
-  },
-  {
-    icon: iconMoney,
-    alt: "Money Icon",
-    title: "More savings means higher rates",
-    content:
-      "The more you save with us, the higher your interest rate will be!",
-  },
-  {
-    icon: iconSecurity,
-    alt: "Security Icon",
-    title: "Security you can trust",
-    content:
-      "We use top of the line encryption to make sure your data and money is always safe.",
-  },
-]
 
 export default function Home() {
   return (
