b2c9ec6852cb0ea7731354b67f5d1fc9184dd7fd
ZhannaZucher <jzucher@gmail.com>
2023-07-31 11:32:07 +0200

    add: SignIn page stucture

--- src/pages/SignIn/SingIn.jsx
+++ src/pages/SignIn/SingIn.jsx
@@ -1,5 +1,14 @@
+import SignInForm from "../../components/SignInForm/SignInForm"
 import "./SignIn.css"
 
 export default function SignIn() {
-  return <div>SignIn PAGE</div>
+  return (
+    <main className="main bg-dark">
+      <section className="sign-in-content">
+        <i className="fa fa-user-circle sign-in-icon"></i>
+        <h1>Sign In</h1>
+        <SignInForm />
+      </section>
+    </main>
+  )
 }
