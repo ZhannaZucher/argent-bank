b06848c5b622d39ca7193ba14869b3b088526b68
ZhannaZucher <jzucher@gmail.com>
2023-08-01 10:54:45 +0200

    mock account data

---
+++ src/data/AccountDataMock.js
@@ -0,0 +1,20 @@
+export const AccountDataMock = [
+  {
+    title: "Checking",
+    deal: 8349,
+    amount: "2,082.79",
+    status: "Available",
+  },
+  {
+    title: "Savings",
+    deal: 6712,
+    amount: "10,928.42",
+    status: "Available",
+  },
+  {
+    title: "Credit Card",
+    deal: 8349,
+    amount: "184.30",
+    status: "Current",
+  },
+]
