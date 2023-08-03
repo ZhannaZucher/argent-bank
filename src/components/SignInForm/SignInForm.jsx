ef26e00cb198936f3c024b5e0dadda7651ff5496
ZhannaZucher <jzucher@gmail.com>
2023-08-03 11:59:50 +0200

    add useState & eventHandlers

--- src/components/SignInForm/SignInForm.jsx
+++ src/components/SignInForm/SignInForm.jsx
@@ -1,18 +1,42 @@
+import { useState } from "react"
 import "./SignInForm.css"
 
 export default function SignInForm() {
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  const [rememberMe, setRememberMe] = useState(false)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(username, password, rememberMe)
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div className="input-wrapper">
         <label htmlFor="username">Username</label>
-        <input type="text" id="username" />
+        <input
+          type="text"
+          id="username"
+          onChange={(e) => setUsername(e.target.value)}
+        />
       </div>
       <div className="input-wrapper">
         <label htmlFor="password">Password</label>
-        <input type="password" id="password" />
+        <input
+          type="password"
+          id="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
       </div>
       <div className="input-remember">
-        <input type="checkbox" id="remember-me" />
+        <input
+          type="checkbox"
+          id="remember-me"
+          onChange={(e) => {
+            setRememberMe(e.target.checked)
+          }}
+        />
         <label htmlFor="remember-me">Remember me</label>
       </div>
       <button className="sign-in-button">Sign In</button>
