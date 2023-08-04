53b549e469da2bf0201a95d0bdc43ee587a83697
ZhannaZucher <jzucher@gmail.com>
2023-08-04 10:32:45 +0200

    add: redux provider

--- src/App.js
+++ src/App.js
@@ -4,18 +4,22 @@ import Home from "./pages/Home/Home"
 import SignIn from "./pages/SignIn/SignIn"
 import User from "./pages/User/User"
 import Footer from "./components/Footer/Footer"
+import { Provider } from "react-redux"
+import { store } from "./app/store"
 
 function App() {
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/user" element={<User />} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/user" element={<User />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </Provider>
   )
 }
 
