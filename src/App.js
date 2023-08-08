0dbfe9223cd6ec5e02a427206b8a3613d6eb702b
ZhannaZucher <jzucher@gmail.com>
2023-08-08 17:09:42 +0200

    change routes to public & protected

--- src/App.js
+++ src/App.js
@@ -6,8 +6,10 @@ import User from "./pages/User/User"
 import Footer from "./components/Footer/Footer"
 import { Provider } from "react-redux"
 import { store } from "./app/store"
+import Layout from "./components/Layout/Layout"
+import RequireAuth from "./components/RequireAuth/RequireAuth"
 
-function App() {
+/*function App() {
   return (
     <Provider store={store}>
       <BrowserRouter>
@@ -16,6 +18,31 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/sign-in" element={<SignIn />} />
           <Route path="/user" element={<User />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </Provider>
+  )
+}
+
+export default App
+*/
+
+function App() {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            {/* public routes */}
+            <Route index element={<Home />} />
+            <Route path="/sign-in" element={<SignIn />} />
+            {/* protected routes */}
+            <Route element={<RequireAuth />}>
+              <Route path="/user" element={<User />} />
+            </Route>
+          </Route>
         </Routes>
         <Footer />
       </BrowserRouter>
