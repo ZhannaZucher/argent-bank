bd812b41a52d9844076ac3dda1ee8fbfd4fd1e26
ZhannaZucher <jzucher@gmail.com>
2023-08-09 09:36:56 +0200

    add loader

--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -1,14 +1,23 @@
+import { useSelector } from "react-redux"
 import SignInForm from "../../components/SignInForm/SignInForm"
 import "./SignIn.css"
+import { selectAuthStatus } from "../../app/selectors"
+import Loader from "../../components/Loader/Loader"
 
 export default function SignIn() {
+  const queryStatus = useSelector(selectAuthStatus)
+  console.log(queryStatus)
   return (
     <main className="main bg-dark">
-      <section className="sign-in-content">
-        <i className="fa fa-user-circle sign-in-icon"></i>
-        <h1>Sign In</h1>
-        <SignInForm />
-      </section>
+      {queryStatus === "fetching" ? (
+        <Loader />
+      ) : (
+        <section className="sign-in-content">
+          <i className="fa fa-user-circle sign-in-icon"></i>
+          <h1>Sign In</h1>
+          <SignInForm />
+        </section>
+      )}
     </main>
   )
 }
