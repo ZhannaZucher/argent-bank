3c5782cdb26e30987612a33ec2108a78b05e5172
ZhannaZucher <jzucher@gmail.com>
2023-08-09 09:37:44 +0200

    authStatus & authError selectors

--- src/app/selectors.js
+++ src/app/selectors.js
@@ -1,1 +1,3 @@
 export const selectCurrentToken = (state) => state.auth.accessToken
+export const selectAuthStatus = (state) => state.auth.status
+export const selectAuthError = (state) => state.auth.error
