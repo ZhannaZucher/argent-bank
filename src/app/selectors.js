6a96d79774977267ccd1211bbb76a5d067f4f224
ZhannaZucher <jzucher@gmail.com>
2023-08-11 08:22:10 +0200

    add rememberMe selector

--- src/app/selectors.js
+++ src/app/selectors.js
@@ -1,3 +1,4 @@
 export const selectCurrentToken = (state) => state.auth.accessToken
 export const selectAuthStatus = (state) => state.auth.status
 export const selectAuthError = (state) => state.auth.error
+export const selectRememberMe = (state) => state.auth.rememberMe
