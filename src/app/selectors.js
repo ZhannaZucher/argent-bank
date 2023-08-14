246407b348f34c7ad43bed8e8dcc697dce050f3a
ZhannaZucher <jzucher@gmail.com>
2023-08-14 17:53:24 +0200

    add main selectors for userSlice

--- src/app/selectors.js
+++ src/app/selectors.js
@@ -1,4 +1,10 @@
+//selectors for authSlice
 export const selectCurrentToken = (state) => state.auth.accessToken
 export const selectAuthStatus = (state) => state.auth.status
 export const selectAuthError = (state) => state.auth.error
 export const selectRememberMe = (state) => state.auth.rememberMe
+
+//selectors for userSlice
+export const selectUserData = (state) => state.user
+export const selectUserStatus = (state) => state.user.status
+export const selectUserError = (state) => state.user.error
