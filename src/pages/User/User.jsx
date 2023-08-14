9f7b28af29f6dd28eddb9c7b33da6f915e603df1
ZhannaZucher <jzucher@gmail.com>
2023-08-14 19:08:36 +0200

    add loader & error handling

--- src/pages/User/User.jsx
+++ src/pages/User/User.jsx
@@ -1,16 +1,38 @@
+import { useSelector } from "react-redux"
 import AccountBlock from "../../components/AccountBlock/AccountBlock"
 import Welcome from "../../components/Welcome/Welcome"
 import { AccountDataMock } from "../../data/AccountDataMock"
 import "./User.css"
+import { selectUserError, selectUserStatus } from "../../app/selectors"
+import Loader from "../../components/Loader/Loader"
 
 export default function User() {
+  const queryStatus = useSelector(selectUserStatus)
+  const error = useSelector(selectUserError)
+  console.log(error)
   return (
-    <main className="main bg-dark">
-      <Welcome />
-      <h2 className="sr-only">Accounts</h2>
-      {AccountDataMock.map((account, index) => {
-        return <AccountBlock key={index} {...account} />
-      })}
-    </main>
+    <>
+      {error?.errorCode ? (
+        <div className="error-block main bg-dark">
+          <p className="text-danger">
+            Oups, something went wrong, <br /> please retry later
+          </p>
+        </div>
+      ) : (
+        <main className="main bg-dark">
+          {queryStatus === "fetching" ? (
+            <Loader />
+          ) : (
+            <>
+              <Welcome />
+              <h2 className="sr-only">Accounts</h2>
+              {AccountDataMock.map((account, index) => {
+                return <AccountBlock key={index} {...account} />
+              })}
+            </>
+          )}
+        </main>
+      )}
+    </>
   )
 }
