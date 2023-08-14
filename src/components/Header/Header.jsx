a3a78b47a2a40af1c3ed06321832d95dc917fa42
ZhannaZucher <jzucher@gmail.com>
2023-08-14 18:17:17 +0200

    add : logout onClick

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,16 @@
 import { Link } from "react-router-dom"
 import "./Header.css"
 import logo from "../../assets/argentBankLogo.png"
-import { useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { selectUserData, selectUserStatus } from "../../app/selectors"
+import { logOut } from "../../features/user/userSlice"
 
 export default function Header() {
-  const isLogged = useSelector(selectUserStatus)
+  const dispatch = useDispatch()
+  //checking for user connected :
+  const isLogged = useSelector(selectUserStatus) === "resolved"
   const user = useSelector(selectUserData)
-  console.log(user.firstName)
+
   return (
     <nav className="main-nav">
       <Link className="main-nav-logo" to="/">
@@ -18,13 +21,20 @@ export default function Header() {
         />
         <h1 className="sr-only">Argent Bank</h1>
       </Link>
-      {isLogged === "resolved" ? (
+      {isLogged ? (
         <div>
           <a href="/profile" className="main-nav-item">
             <i className="fa fa-user-circle"></i>
             {user.firstName}
           </a>
-          <a href="/" className="main-nav-item">
+          <a
+            href="/"
+            className="main-nav-item"
+            onClick={(e) => {
+              e.preventDefault()
+              dispatch(logOut)
+            }}
+          >
             <i className="fa fa-sign-out"></i>
             Sign Out
           </a>
